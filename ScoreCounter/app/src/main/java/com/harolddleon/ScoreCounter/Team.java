6469d05f87eea1e54fad0790ec29c3a5b6a3dc04
HaroldDLeon <HaroldDLeon@users.noreply.github.com>
2018-09-19 22:43:28 -0400

    Improved readability of teams and colors.

--- ScoreCounter/app/src/main/java/com/harolddleon/ScoreCounter/Team.java
+++ ScoreCounter/app/src/main/java/com/harolddleon/ScoreCounter/Team.java
@@ -1,14 +1,20 @@
 package com.harolddleon.ScoreCounter;
 
+
 public class Team {
-    static final String[] teams = {"Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets",
-            "Chicago Bulls", "Cleveland Cavaliers", "Dallas Mavericks", "Denver Nuggets", "Detroit Pistons",
-            "Golden State Warriors", "Houston Rockets", "Indiana Pacers", "Los Angeles Clippers", "Los Angeles Lakers",
-            "Memphis Grizzlies", "Miami Heat", "Milwaukee Bucks", "Minnesota Timberwolves", "New Orleans Pelicans", "New York Knicks", "Oklahoma City Thunder", "" +
-            "Orlando Magic", "Philadelphia 76ers", "Phoenix Suns", "Portland Trail Blazers", "Sacramento Kings", "San Antonio Spurs", "Toronto Raptors", "Utah Jazz", "" +
-            "Washington Wizards"};
-    static final String[] colors = {"#E03A3E", "#007A33", "#000000", "#1D1160", "#CE1141", "#6F263D", "#00538C", "#0E2240", "#C8102E", "#006BB6", "#CE1141", "##FDBB30",
-                    "#C8102E", "#552583", "#5D76A9", "#38B5E6", "#00471B", "#0C2340", "#85714D", "#006BB6", "#006BB6", "#006BB6", "#006BB6", "#E56020", "#E03A3E", "#C4CED4", "#CE1141",
-                    "#F9A01B", "#C4CED4"};
+    static final String[] teams = {
+            "Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets", "Chicago Bulls",
+            "Cleveland Cavaliers", "Dallas Mavericks", "Denver Nuggets", "Detroit Pistons", "Golden State Warriors",
+            "Houston Rockets", "Indiana Pacers", "Los Angeles Clippers", "Los Angeles Lakers", "Memphis Grizzlies",
+            "Miami Heat", "Milwaukee Bucks", "Minnesota Timberwolves", "New Orleans Pelicans", "New York Knicks",
+            "Oklahoma City Thunder", "Orlando Magic", "Philadelphia 76ers", "Phoenix Suns", "Portland Trail Blazers",
+            "Sacramento Kings", "San Antonio Spurs", "Toronto Raptors", "Utah Jazz", "Washington Wizards"};
+
+    static final String[] colors = {"#E03A3E", "#007A33", "#000000", "#1D1160", "#CE1141",
+                                    "#6F263D", "#00538C", "#0E2240", "#C8102E", "#006BB6",
+                                    "#CE1141", "##FDBB30", "#C8102E", "#552583", "#5D76A9",
+                                    "#38B5E6", "#00471B", "#00843D", "#85714D", "#006BB6",
+                                    "#007AC1", "#0077C0", "#006BB6", "#E56020", "#E03A3E",
+                                    "#542E91", "#C4CED4", "#CE1141", "#F9A01B", "#C4CED4"};
 
 }
