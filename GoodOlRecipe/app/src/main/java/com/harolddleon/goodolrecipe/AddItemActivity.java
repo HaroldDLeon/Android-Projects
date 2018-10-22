1a4d91c1f3da75377668dce3acdc7fb1d9bfa951
Harold De Leon <harold.dleon@hotmail.com>
2018-10-22 18:28:56 -0400

    Updated layout for AddItemActivity

--- GoodOlRecipe/app/src/main/java/com/harolddleon/goodolrecipe/AddItemActivity.java
+++ GoodOlRecipe/app/src/main/java/com/harolddleon/goodolrecipe/AddItemActivity.java
@@ -15,16 +15,12 @@ public class AddItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_item);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        toolbar.setTitle(R.string.activity_add_item);
+        setSupportActionBar(toolbar);
+
     }
 
 }
