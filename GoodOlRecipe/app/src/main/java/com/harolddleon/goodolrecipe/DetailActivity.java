b5554b63e4f8993a60fadfe7ebec724f25b69db0
Harold De Leon <harold.dleon@hotmail.com>
2018-10-23 02:38:39 -0400

    Temporarily removed transparency from Toolbar

--- GoodOlRecipe/app/src/main/java/com/harolddleon/goodolrecipe/DetailActivity.java
+++ GoodOlRecipe/app/src/main/java/com/harolddleon/goodolrecipe/DetailActivity.java
@@ -1,9 +1,12 @@
 package com.harolddleon.goodolrecipe;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -32,6 +35,10 @@ public class DetailActivity extends AppCompatActivity {
         recipeDescription.setText(intent.getStringExtra("description"));
 
         toolbar.setTitle(recipeTitle.getText());
+//        Only Enable if the images of the recipes were changed
+//        toolbar.setBackground(null);
+//        toolbar.setTitleTextColor(Color.GRAY);
+
         setSupportActionBar(toolbar);
 
 
