fe6be4a1219ecf43a5f04e6892127e10ee61d42e
Harold De Leon <harold.dleon@hotmail.com>
2018-11-21 15:55:56 -0500

    Updated conditions to include new backgrounds

--- ScoreCounter/app/src/main/java/com/harolddleon/ScoreCounter/MainActivity.java
+++ ScoreCounter/app/src/main/java/com/harolddleon/ScoreCounter/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-        preferences = PreferenceManager.getDefaultSharedPreferences(this);
         setContentView(R.layout.activity_main);
 
         textViewHomeScore = findViewById(R.id.textView_HomeTeamScore);
@@ -43,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
         buttonViewHome = findViewById(R.id.buttonView_HomeTeam);
         buttonViewAway = findViewById(R.id.buttonView_AwayTeam);
 
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
         preferences.registerOnSharedPreferenceChangeListener(this);
 
         buttonViewAway.setOnClickListener(this);
@@ -92,9 +92,6 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         switch (key) {
-            case "night_mode":
-                recreate();
-                break;
             case "home_text":
                 updateTitles();
                 break;
@@ -102,22 +99,37 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
                 updateTitles();
                 break;
         }
-        updateTitles();
+        recreate();
     }
 
     private void updateDrawables() {
         ViewGroup main_layout = findViewById(R.id.main_layout);
+        String sport = preferences.getString("sports_list", "Basketball");
         int nightModeFlags =
                 getResources().getConfiguration().uiMode &
                         Configuration.UI_MODE_NIGHT_MASK;
         switch (nightModeFlags) {
             case Configuration.UI_MODE_NIGHT_YES:
                 main_layout.setBackground(getResources().getDrawable(R.drawable.ic_background_heat_court_dark));
+                switch (sport) {
+                    case "Basketball":
+                        main_layout.setBackground(getResources().getDrawable(R.drawable.ic_background_heat_court_dark));
+                        break;
+                    case "Hockey":
+                        main_layout.setBackground(getResources().getDrawable(R.drawable.ic_background_hockey_dark));
+                        break;
+                }
                 break;
 
             case Configuration.UI_MODE_NIGHT_NO:
-                main_layout.setBackground(getResources().getDrawable(R.drawable.ic_background_heat_court));
-                break;
+                switch (sport) {
+                    case "Basketball":
+                        main_layout.setBackground(getResources().getDrawable(R.drawable.ic_background_heat_court));
+                        break;
+                    case "Hockey":
+                        main_layout.setBackground(getResources().getDrawable(R.drawable.ic_background_hockey));
+                        break;
+                }
         }
     }
 
